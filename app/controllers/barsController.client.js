a90f3dac0c123834c8db17ea7caab123eec9f826
Michael Krebs <michael@krb.io>
2015-11-13 04:19:47 +0000

    logout routed

--- app/controllers/barsController.client.js
+++ app/controllers/barsController.client.js
@@ -2,18 +2,43 @@
 
 $(document).ready(function(){
 
+	// sets username at navbar
+	$.get("/api/:id", function(data) {
+		$('#username').text("Hello, " + data.displayName + "!");
+	});
 	
 	// take search input when submitted
 	$('#searchBtn').click(function() {
 		var searchText = $('#search').val();
 		
-		// make get request to yelp controller
+		// get info about bars in the area from yelp
 		$.get("/api/yelp/" + searchText, function(data) {
 			var bars = JSON.parse(data).businesses;
 			populate(bars);
 		});
+		
+		//ajax search? <- lag city
+		/*$("input").keyup(function() {
+			$.ajax({
+	      type: "GET",
+	      url: '/api/yelp/' + searchText,
+	      contentType: "application/json; charset=utf-8",
+	      async: false,
+	      dataType: "json",
+	      success: function (data) {
+	        $.each(data, function(i, item) {
+	          $('div.searchBox').html(item.extract);
+	          var bars = JSON.parse(data).businesses;
+			      populate(bars);
+	      },
+	      error: function (errorMessage) {
+	      }
+	    });
+    }).keyup();
+    */
 	});
 	
+	// update who's going list when clicked
   	$('#barList').on('click', '.goingButton', function(){
   		var barId = this.id;
   		$.post('/api/bar/' + barId, function(data){
@@ -32,27 +57,25 @@ $(document).ready(function(){
 			// get large rather than tiny img from yelp
 			bar.largeImg = bar.image_url.slice(0, -6) + "l.jpg";
 			
+			// create each bar card w/ relevant info
 			$.get('/api/bar/' + bar.id, function(numberGoing) {
 				$('#barList').append(
-				"<div class='col s12 m6'>" +
-				  "<div class='card medium'>" +
-					"<div class='card-image'>" +
-					  "<img src='" + bar.largeImg + "'>" +
-					  "<span class='card-title'>" + bar.name +"</span>" +
-					"</div>" +
-					"<div class='card-content'>" +
-						"<p class='description truncate'>" + bar.snippet_text + "</p>" +
-						"<a href='" + bar.url + "' class='btn'>Yelp</a>" +
-						"<a href='#' id='" + bar.id + "' class='btn right-align goingButton'>" + numberGoing +" I'm Going</a>" +  
-				  	"</div>" +
-				"</div>" +
-				"</div>"
-			)
-				
-			})
-			
-		
-		})
+  				"<div class='col s12 m6'>" +
+  				  "<div class='card medium'>" +
+  					"<div class='card-image'>" +
+  					  "<img src='" + bar.largeImg + "'>" +
+  					  "<span class='card-title'>" + bar.name +"</span>" +
+  					"</div>" +
+  					"<div class='card-content'>" +
+  						"<p class='description truncate'>" + bar.snippet_text + "</p>" +
+  						"<a href='" + bar.url + "' class='btn'>Yelp</a>" +
+  						"<a href='#' id='" + bar.id + "' class='btn right-align goingButton'>" + numberGoing +" I'm Going</a>" +  
+  				  	"</div>" +
+  				"</div>" +
+  				"</div>"
+			  );
+			});
+		});
 	}
 
 });
